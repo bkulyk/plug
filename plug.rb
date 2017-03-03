d4fde338ce31834ba06c00d5a929b1f917e0c596
Brian Kulyk <brian@kulyk.ca>
2017-03-03 04:54:19 +0000

    changes

--- plug.rb
+++ plug.rb
@@ -4,13 +4,18 @@ require 'rpi_gpio'
 PIN_NUM = 21
 
 RPi::GPIO.set_numbering :bcm
-RPi::GPIO.setup PIN_NUM, :as => :output, :initialize => :low
+# RPi::GPIO.setup PIN_NUM, :as => :output, :initialize => :low
+
+Pins = [26, 19, 6, 5]
+Pins.each do |pin|
+  RPi::GPIO.setup pin, as: :output#, initialize: :low
+end
 
 # this needs to be defined before Sinatra does it's initialization or we won't get the chance to run this
 at_exit do
   # close the pin "device" so it can be used again later
   # the PiPiper does not seem to have any way to close the pin device, so we'll do it manually
-  release_pin
+  RPi::GPIO.reset
 end
 
 require 'sinatra'
@@ -19,16 +24,22 @@ set :port, 80
 
 set :static_cache_control, [:public, max_age: 1]
 
-get '/on' do
-  RPi::GPIO.set_high PIN_NUM
-  take_photo
-  redirect '/#on'
-end
+get '/toggle' do
+  pin = params[:pin].to_i
+  unless Pins.include? pin
+    puts 'invalid pin'
+    redirect "/#error"
+    return
+  end
 
-get '/off' do
-  RPi::GPIO.set_low PIN_NUM
-  take_photo
-  redirect '/#off'
+  RPi::GPIO.set_high pin
+
+  sleep 0.5
+
+  RPi::GPIO.set_low pin
+  
+#  take_photo
+  redirect "/#toggle-#{pin}"
 end
 
 get '/' do
@@ -41,8 +52,3 @@ def take_photo
   `raspistill -o #{path}  -hf -vf --nopreview --width 640 --height 480`
 end
 
-def release_pin
-  RPi::GPIO.reset
-end
-
-take_photo
